857b235af72457fa34f403a4f736b51d9ed5ab76
fatmaelhot <fatmaelhot289@gmail.com>
2024-02-23 07:57:41 +0200

    add service & validation

--- src/app/Components/User/home/home.component.ts
+++ src/app/Components/User/home/home.component.ts
@@ -1,8 +1,6 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { AuthService } from '../../../Services/auth.service';
-import { UserReviewService } from '../../../Services/user-review.service';
-import { BookService } from '../../../Services/book.service';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 @Component({
